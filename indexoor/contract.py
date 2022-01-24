75eeb891453bbd4778e5e184d522d54329e3199b
theo <theodorusib@gmail.com>
2022-01-24 13:27:12 +0000

    enable for 1155

--- indexoor/contract.py
+++ indexoor/contract.py
@@ -1,3 +1,5 @@
+from typing import Optional
+from pydantic import BaseModel
 import json
 from web3 import Web3
 
@@ -47,6 +49,7 @@ class _Contract:
 
 
 class Contract(_Contract):
+    @property
     def name(self) -> str:
         return self.contract.functions.name().call()
 
@@ -55,30 +58,74 @@ class Contract(_Contract):
             return ERC721(self.w3, self.addr)
 
         if self.get_erc_type() == "ERC1155":
-            return ERC721(self.w3, self.addr)
+            return ERC1155(self.w3, self.addr)
         raise Exception("Implement this")
+
+
+class ERC721Model(BaseModel):
+    name: str
+    symbol: str
+    totalSupply: str
+    tokenURI: Optional[str]
 
 
 class ERC721(Contract):
     def __init__(self, w3: Web3, addr, abi=erc721_abi):
         super().__init__(w3, addr, abi=abi)
 
+    @property
     def symbol(self) -> str:
         return self.contract.functions.symbol().call()
 
-    def totalSupply(self) -> str:
+    @property
+    def total_supply(self) -> str:
         return self.contract.functions.totalSupply().call()
 
-    def tokenURI(self, id: int) -> str:
+    def token_uri(self, id: int) -> str:
         return self.contract.functions.tokenURI(id).call()
+
+    def get_contract_info(self):
+        return ERC721Model(
+            name=self.name,
+            symbol=self.symbol,
+            totalSupply=self.total_supply,
+        )
+
+    def get_token_information(self, token_id):
+        return ERC721Model(
+            name=self.name,
+            symbol=self.symbol,
+            totalSupply=self.total_supply,
+            tokenURI=self.token_uri(token_id),
+        )
+
+
+class ERC1155Model(BaseModel):
+    name: str
+    symbol: str
+    uri: Optional[str]
 
 
 class ERC1155(Contract):
     def __init__(self, w3: Web3, addr, abi=erc1155_abi):
         super().__init__(w3, addr, abi=abi)
 
+    @property
     def symbol(self) -> str:
         return self.contract.functions.symbol().call()
 
-    def uri(self, id: int) -> str:
-        return self.contract.functions.tokenURI(id).call()
+    def uri(self, token_id: int) -> str:
+        return self.contract.functions.uri(token_id).call()
+
+    def get_contract_info(self):
+        return ERC1155Model(
+            name=self.name,
+            symbol=self.symbol,
+        )
+
+    def get_token_information(self, token_id):
+        return ERC1155Model(
+            name=self.name,
+            symbol=self.symbol,
+            uri=self.uri(token_id),
+        )
--- indexoor/web.py
+++ indexoor/web.py
@@ -22,23 +22,12 @@ app = Flask(__name__)
 
 @app.get("/assets/<string:addr>")
 def get_contract_info(addr: str):
-    contract = Contract(eth.w3, addr, abi).get_instance()
-    payload = ERC721(
-        name=contract.name(),
-        symbol=contract.symbol(),
-        totalSupply=contract.totalSupply(),
-    )
-
-    return payload.dict()
+    contract = Contract(eth.w3, addr.lower(), abi).get_instance()
+    return contract.get_contract_info().dict()
 
 
 @app.get("/assets/<string:addr>/<int:token_id>")
 def get_token_info(addr, token_id):
-    contract = Contract(eth.w3, addr, abi).get_instance()
-    payload = ERC721(
-        name=contract.name(),
-        symbol=contract.symbol(),
-        totalSupply=contract.totalSupply(),
-        tokenURI=contract.tokenURI(token_id),
-    )
-    return payload.dict()
+
+    contract = Contract(eth.w3, addr.lower(), abi).get_instance()
+    return contract.get_token_information(token_id).dict()
